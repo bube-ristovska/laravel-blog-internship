672dde09fb6c69c71fa9eb578ad8f608791b68f8
bube-ristovska <96651165+bube-ristovska@users.noreply.github.com>
2024-06-15 16:16:34 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,6 @@ use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::post('newsletter', function (){
     request()->validate(['email'=>'required|email']);
     $mailchimp = new \MailchimpMarketing\ApiClient();
